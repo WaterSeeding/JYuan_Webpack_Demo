c4f0fbd717a4390c21a6bfc0dc59a4667a146ecc
JYuan <13826506407@139.com>
2025-01-13 18:38:03 +0800

    feat: 自定义plugin

--- webpack_plugin.js
+++ webpack_plugin.js
@@ -5,13 +5,16 @@ class MyPlugin {
   }
 
   apply(compiler) {
+    compiler.hooks.done.tap("MyPlugin", (Compilation) => {
+      console.log("MyPlugin: Compilation finished!");
+    });
     compiler.hooks.emit.tapAsync("MyPlugin", (compilation, callback) => {
       console.log("这是一个示例插件！");
 
       Object.keys(compilation.assets).forEach((filename) => {
         console.log("filename", filename);
         // 只处理 .tsx 文件
-        if (filename.endsWith(".tsx")) {
+        if (filename.endsWith(".js")) {
           // 获取当前文件的内容
           const asset = compilation.assets[filename];
           const source = asset.source();
